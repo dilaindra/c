30cac286e2a09b92cdfff6ab23b7128c0f98ad27
dilaindra <103862262+dilaindra@users.noreply.github.com>
2022-04-17 05:41:35 +0700

    Add files via upload

---
+++ Kapital di Awal Kalimat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+	char kata[255];
+	scanf(" %[^\n]", &kata);
+	int i;
+	int panjang;
+	
+	panjang =strlen(kata);
+	
+	for(i=0; i<panjang; i++){
+		if(i == 0){
+			kata[i] = toupper(kata[i]);
+		}
+		while(kata[i] != ' '){
+			i++;
+		}
+		if(kata[i] == ' '){
+			kata[i+1] = toupper(kata[i+1]);
+		}
+		printf("%d\n", i);
+	}
+	
+	printf("%s", kata);
+	return 0;
+}
